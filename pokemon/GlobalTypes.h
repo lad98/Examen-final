399e1e18586c8adccb00b520a60469b66e95a2ce
lad98 <ldanielajimenezm@gmail.com>
2025-05-28 21:18:14 -0500

    Create GlobalTypes.h

---
+++ pokemon/GlobalTypes.h
@@ -0,0 +1,15 @@
+
+#ifndef GLOBAL_TYPES_H
+#define GLOBAL_TYPES_H
+
+
+enum class StatusEffectType {
+    None,
+    Poisoned,   
+    Burned,     
+    Paralyzed,  
+    Asleep,     
+    Frozen,     
+};
+
+#endif 
